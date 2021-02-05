bfdf6b924a240df64886cee55961505b93dcdbd3
Do hoai thanh <hoaithanhdo@gmail.com>
2021-02-05 11:31:32 +0700

    moi hoc git

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php
 ECHO "Hello World!<br>";
 echo "Hello World!<br>";
-EcHo "Hello tonydo - aws that la don gian:version new<br>";
+EcHo "Hello tonydo - aws that la don gian:version new 222222222222222<br>";
 ?>
 
 </body>
